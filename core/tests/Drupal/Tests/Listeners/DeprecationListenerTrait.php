b3bd2401150b0cfef0f111381556feca9e43a2bb
Pooja Ganjage <pooja.ganjage@asentech.com>
2020-11-02 13:48:18 +0530

    Core Files Added

--- core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
+++ core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
@@ -74,8 +74,8 @@ trait DeprecationListenerTrait {
       '%The "PHPUnit\\\\Framework\\\\TestCase::addWarning\(\)" method is considered internal%',
       // The following deprecations were not added as part of the original
       // issues and thus were not addressed in time for the 9.0.0 release.
-      '%The entity link url update for the "\w+" view is deprecated in drupal:9\.0\.0 and is removed from drupal:10\.0\.0\. Module-provided Views configuration should be updated to accommodate the changes described in the change record\. See https://www\.drupal\.org/node/2857891%',
-      '%The operator defaults update for the "\w+" view is deprecated in drupal:9\.0\.0 and is removed from drupal:10\.0\.0\. Module-provided Views configuration should be updated to accommodate the changes described in the change record\. See https://www\.drupal\.org/node/2869168%',
+      '%The entity link url update for the "\w+" view is deprecated in drupal:9.0.0 and is removed from drupal:10.0.0. Module-provided Views configuration should be updated to accommodate the changes described in the change record. See https://www.drupal.org/node/2857891%',
+      '%The operator defaults update for the "\w+" view is deprecated in drupal:9.0.0 and is removed from drupal:10.0.0. Module-provided Views configuration should be updated to accommodate the changes described in the change record. See https://www.drupal.org/node/2869168%',
     ];
     return (bool) preg_filter($dynamic_skipped_deprecations, '$0', $message);
   }
